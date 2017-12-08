eb1f811ea04c1fcce67e3db1a3eb116af3a42d88
dongzeus <dongzeus@163.com>
2017-12-08 13:58:38 +0800

    modify

--- models_conv.py
+++ models_conv.py
@@ -211,6 +211,7 @@ class N_pair_loss(torch.nn.Module):
             Djk = dis[:,i].clone()
             Djk[i] = 0
 
+
             loss_i = torch.log(torch.sum(torch.exp(margin * torch.autograd.Variable(torch.ones(Dik.size())).cuda() - Dik) + torch.exp(
                 margin * torch.autograd.Variable(torch.ones(Djk.size())).cuda() - Djk), dim=0)) + Dij
             if torch.norm(loss_i, p=1).data[0] < 0:
