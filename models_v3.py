aae5180ceff5a224398360717a9abe84bfc44c91
dongzeus <dongzeus@163.com>
2017-12-10 15:34:38 +0800

    Revert "modify"
    This reverts commit 8d7cbbaa5e88facaa1c69170fb3fd42577478869.

--- models_v3.py
+++ models_v3.py
@@ -47,7 +47,6 @@ class VAMetric_conv(nn.Module):
 
         self.vLSTM = FeatLSTM(1024,512,128)
         self.aLSTM = FeatLSTM(128,128,128)
-        self.dp = nn.Dropout(p=0.2)
 
         self.vfc1 = nn.Linear(in_features=1024, out_features=512)
         self.vfc2 = nn.Linear(in_features=512, out_features=128)
@@ -56,7 +55,7 @@ class VAMetric_conv(nn.Module):
         # self.mp = nn.MaxPool1d(kernel_size=4)
         self.conv2 = nn.Conv1d(in_channels=32, out_channels=32, kernel_size=8, stride=1)
         self.fc3 = nn.Linear(in_features=32 * 113, out_features=1024)
-        self.fc4 = nn.Linear(in_features=1024, out_features=2)
+        self.fc4 = nn.Linear(in_features=1024, out_features=512)
         self.fc5 = nn.Linear(in_features=512, out_features=128)
         self.fc6 = nn.Linear(in_features=128, out_features=2)
         self.init_params()
@@ -87,15 +86,13 @@ class VAMetric_conv(nn.Module):
         vafeat = self.conv2(vafeat)
 
         vafeat = vafeat.view([vafeat.size(0), -1])
-        vafeat = self.dp(vafeat)
-
         vafeat = self.fc3(vafeat)
         vafeat = F.relu(vafeat)
         vafeat = self.fc4(vafeat)
-        # vafeat = F.relu(vafeat)
-        # vafeat = self.fc5(vafeat)
-        # vafeat = F.relu(vafeat)
-        # vafeat = self.fc6(vafeat)
+        vafeat = F.relu(vafeat)
+        vafeat = self.fc5(vafeat)
+        vafeat = F.relu(vafeat)
+        vafeat = self.fc6(vafeat)
 
 
         result = F.softmax(vafeat)
