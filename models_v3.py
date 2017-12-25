a42b91e2fdbc73e15fe02749b4e7ef7ce6f77a3e
dongzeus <dongzeus@163.com>
2017-12-26 00:14:03 +0800

    lstm

--- models_v3.py
+++ models_v3.py
@@ -71,8 +71,8 @@ class VAMetric_conv(nn.Module):
 
     def forward(self, vfeat, afeat):
 
-        #vfeat = self.vLSTM(vfeat)
-        #afeat = self.aLSTM(afeat)
+        vfeat = self.vLSTM(vfeat)
+        afeat = self.aLSTM(afeat)
 
         vfeat = self.vfc1(vfeat)
         vfeat = F.relu(vfeat)
