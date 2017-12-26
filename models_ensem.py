05fc9896b319d852f2dc77ebd7c2316101375112
dongzeus <dongzeus@163.com>
2017-12-26 21:44:38 +0800

    add ensemble

---
+++ models_ensem.py
@@ -0,0 +1,176 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import numpy as np
+
+
+
+class FeatLSTM(nn.Module):
+    def __init__(self, input_size=1024, hidden_size=128, out_size=128):
+        super(FeatLSTM, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.out_size = out_size
+        self.lstm1 = nn.LSTMCell(input_size, hidden_size)
+        self.lstm2 = nn.LSTMCell(hidden_size, out_size)
+
+    def forward(self, feats):
+        h_t = Variable(torch.zeros(feats.size(0), self.hidden_size).float(), requires_grad=False)
+        c_t = Variable(torch.zeros(feats.size(0), self.hidden_size).float(), requires_grad=False)
+        h_t2 = Variable(torch.zeros(feats.size(0), self.out_size).float(), requires_grad=False)
+        c_t2 = Variable(torch.zeros(feats.size(0), self.out_size).float(), requires_grad=False)
+
+        if feats.is_cuda:
+            h_t = h_t.cuda()
+            c_t = c_t.cuda()
+            h_t2 = h_t2.cuda()
+            c_t2 = c_t2.cuda()
+
+        for _, feat_t in enumerate(feats.chunk(feats.size(1), dim=1)):
+            h_t, c_t = self.lstm1(feat_t[:, 0, :], (h_t, c_t))
+            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
+            if _ == 0:
+                stream = h_t2.view(h_t2.size(0), 1, -1)
+            else:
+                stream = torch.cat((stream, h_t2.view(h_t2.size(0), 1, -1)), dim=1)
+        # aggregated feature
+
+        return stream
+
+
+class VAMetric_conv(nn.Module):
+    def __init__(self, framenum=120):
+        super(VAMetric_conv, self).__init__()
+
+        self.vLSTM = FeatLSTM(1024, 512, 128)
+        self.aLSTM = FeatLSTM(128, 128, 128)
+
+        self.vfc1 = nn.Linear(in_features=1024, out_features=512)
+        self.vfc2 = nn.Linear(in_features=512, out_features=128)
+        self.afc1 = nn.Linear(in_features=128, out_features=128)
+        self.afc2 = nn.Linear(in_features=128, out_features=128)
+
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(2, 128), stride=128)  # output bn*32*120
+        # self.mp = nn.MaxPool1d(kernel_size=4)
+        self.dp = nn.Dropout(0.5)
+        self.conv2 = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=8, stride=1)  # output bn*32*113
+        self.fc3 = nn.Linear(in_features=32 * 120, out_features=4096)
+        self.fc4 = nn.Linear(in_features=4096, out_features=1024)
+        self.fc5 = nn.Linear(in_features=1024, out_features=2)
+        self.fc6 = nn.Linear(in_features=128, out_features=2)
+        self.init_params()
+
+    def init_params(self):
+        for m in self.modules():
+
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_normal(m.weight)
+                nn.init.constant(m.bias, 0)
+
+    def forward(self, vfeat, afeat):
+
+        # vfeat = self.vLSTM(vfeat)
+        # afeat = self.aLSTM(afeat)
+
+        vfeat = self.vfc1(vfeat)
+        vfeat = F.relu(vfeat)
+        vfeat = self.vfc2(vfeat)
+        vfeat = F.relu(vfeat)
+
+        vfeat = vfeat.view(vfeat.size(0), 1, 1, -1)
+        afeat = afeat.view(afeat.size(0), 1, 1, -1)
+
+        vafeat = torch.cat((vfeat, afeat), dim=2)
+        vafeat = self.conv1(vafeat)
+        vafeat = self.dp(vafeat)
+        # vafeat = vafeat.view(vafeat.size(0), vafeat.size(1), -1)
+        # vafeat = self.conv2(vafeat)
+        vafeat = self.dp(vafeat)
+        vafeat = vafeat.view([vafeat.size(0), -1])
+        vafeat = self.fc3(vafeat)
+        vafeat = F.relu(vafeat)
+        vafeat = self.fc4(vafeat)
+        vafeat = F.relu(vafeat)
+        vafeat = self.fc5(vafeat)
+
+
+        result = F.softmax(vafeat)
+
+        return result
+
+
+#
+# class conv_loss_dqy(torch.nn.Module):
+#     def __init__(self):
+#         super(conv_loss_dqy, self).__init__()
+#
+#     def forward(self, sim, label):
+#         length = len(sim[:, 1])
+#         loss1 = torch.mean((1 - label) * sim[:, 1])
+#         loss2 = 1 - torch.mean(torch.pow((1 - label) * sim[:, 0], 2))
+#         loss3 = torch.mean(label * sim[:, 0])
+#         loss4 = 1 - torch.mean(torch.pow(label * sim[:, 1], 2))
+#
+#         # loss3 = 2.2 - (torch.mean(sim[0:length / 2 - 1]) - torch.mean(sim[length / 2:length - 1]))
+#         # loss3 = 1 - torch.mean(torch.abs(sim[:, 0] - sim[:, 1]))
+#
+#         # length = len(sim)
+#         # loss1 = 1 - torch.mean(sim[0:length / 2 - 1])
+#         # loss2 = 1 + torch.mean(sim[length / 2:length - 1])
+#         # loss3 = 2 - (torch.mean(sim[0:length / 2 - 1]) - torch.mean(sim[length / 2:length - 1]))
+#         #
+#         print(list(loss1.data)[0],list(loss2.data)[0],list(loss3.data)[0],list(loss4.data)[0])
+#         return loss1 + 1.3*loss2 + loss3 + loss4
+
+class conv_loss_dqy(torch.nn.Module):
+    def __init__(self):
+        super(conv_loss_dqy, self).__init__()
+
+    def forward(self, sim, label):
+        length = len(sim[:, 1])
+        loss1 = torch.mean(torch.pow((1 - label) * sim[:, 1], 2))
+        loss2 = torch.mean(torch.pow(label * sim[:, 0], 2))
+        # loss3 = 2.2 - (torch.mean(sim[0:length / 2 - 1]) - torch.mean(sim[length / 2:length - 1]))
+        loss3 = 0.9 - torch.mean(torch.abs(sim[:, 0] - sim[:, 1]))
+        return 1 * loss1 + 1 * loss2 + 0 * loss3
+
+
+#
+# class N_pair_loss(torch.nn.Module):
+#     def __init__(self):
+#         super(N_pair_loss, self).__init__()
+#
+#     def forward(self, dis, margin=1):
+#         bn = dis.size()[0]
+#         loss = 0
+#         for i in range(bn):
+#
+#             Dij = dis[i, i]
+#             Dik = dis[i, :].clone()
+#             Dik[i] = 0
+#             Djk = dis[:, i].clone()
+#             Djk[i] = 0
+#             margin_ = margin * torch.autograd.Variable(torch.ones(Dik.size())).cuda()
+#             loss_i = torch.log(torch.sum(torch.exp(margin_ - Dik) + torch.exp(margin_ - Djk), dim=0)) + Dij
+#             if torch.norm(loss_i, p=1).data[0] < 0:
+#                 continue
+#             else:
+#                 loss_i = torch.pow(loss_i, 2)
+#                 loss = loss + loss_i
+#         loss = loss / (2 * bn)
+#
+#         return loss
+#
+class N_pair_loss(torch.nn.Module):
+    def __init__(self):
+        super(N_pair_loss, self).__init__()
+
+    def forward(self, sim_0, sim_1):
+        bn = sim_0.size()[0]
+        loss1 = torch.mean(torch.diag(sim_1))
+        sim_0 = sim_0 - torch.diag(torch.diag(sim_0))
+        # loss2 = torch.mean(torch.max(sim,dim=1)[0])
+        loss2 = torch.mean(torch.mean(sim_0, dim=1), dim=0)
+
+        return loss1 + loss2
---
+++ train_ensem.py
@@ -0,0 +1,314 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import argparse
+import random
+import time
+import os
+import numpy as np
+from optparse import OptionParser
+
+import torch
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.optim.lr_scheduler import LambdaLR as LR_Policy
+
+import models_ensem as models
+from dataset_ensem import VideoFeatDataset
+from tools.config_tools import Config
+from tools import utils
+
+import matplotlib as mpl
+
+mpl.use('Agg')
+
+from matplotlib import pyplot as plt
+
+parser = OptionParser()
+parser.add_option('--config',
+                  type=str,
+                  help="training configuration",
+                  default="./configs/train_config.yaml")
+
+(opts, args) = parser.parse_args()
+assert isinstance(opts, object)
+opt = Config(opts.config)
+print(opt)
+
+if opt.checkpoint_folder is None:
+    opt.checkpoint_folder = 'checkpoints'
+
+# make dir
+if not os.path.exists(opt.checkpoint_folder):
+    os.system('mkdir {0}'.format(opt.checkpoint_folder))
+
+tds_ls = []
+for i in range(opt.model_number):
+    tds_ls.append(VideoFeatDataset(root=opt.data_dir, flist=opt.flist, test_list=opt.test_flist,
+                                   test_number=opt.test_number, bagging=True))
+
+# =================== creat test set before import evaluate ===================
+import evaluate_ensem as evaluate
+
+# =================== creat test set before import evaluate ===================
+
+
+
+print('number of train samples is: {0}'.format(len(tds_ls[0])))
+print('finished loading data')
+
+if opt.manualSeed is None:
+    opt.manualSeed = random.randint(1, 10000)
+
+if torch.cuda.is_available() and not opt.cuda:
+    print("WARNING: You have a CUDA device, so you should probably run with \"cuda: True\"")
+    torch.manual_seed(opt.manualSeed)
+else:
+    if int(opt.ngpu) == 1:
+        print('so we use 1 gpu to training')
+        print('setting gpu on gpuid {0}'.format(opt.gpu_id))
+
+        if opt.cuda:
+            os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpu_id
+            torch.cuda.manual_seed(opt.manualSeed)
+            cudnn.benchmark = True
+print('Random Seed: {0}'.format(opt.manualSeed))
+
+
+# training function for metric learning
+def train(train_loader, model, criterion, optimizer, epoch, opt, num):
+    """
+    train for one epoch on the training set
+    """
+    batch_time = utils.AverageMeter()
+    losses = utils.AverageMeter()
+
+    # training mode
+    model.train()
+
+    end = time.time()
+
+    global positive_rec
+    global negative_rec
+    global loss_rec
+
+    for i, (vfeat, afeat) in enumerate(train_loader):
+        # shuffling the index orders
+        bz = vfeat.size()[0]
+        orders = np.arange(bz).astype('int32')
+        shuffle_orders = orders.copy()
+        np.random.shuffle(shuffle_orders)
+
+        # creating a new data with the shuffled indices
+        afeat2 = afeat[torch.from_numpy(shuffle_orders).long()].clone()
+
+        # concat the vfeat and afeat respectively
+        afeat0 = torch.cat((afeat, afeat2), 0)
+        vfeat0 = torch.cat((vfeat, vfeat), 0)
+
+        # generating the labels
+        # 1. the labels for the shuffled feats
+        label1 = (orders == shuffle_orders + 0).astype('float32')
+        target1 = torch.from_numpy(label1)
+
+        # 2. the labels for the original feats
+        label2 = label1.copy()
+        label2[:] = 1
+        target2 = torch.from_numpy(label2)
+
+        # concat the labels together
+        target = torch.cat((target2, target1), 0)
+        target = 1 - target
+
+        # transpose the feats
+        # vfeat0 = vfeat0.transpose(2, 1)
+        # afeat0 = afeat0.transpose(2, 1)
+
+
+        # put the data into Variable
+        vfeat_var = Variable(vfeat0)
+        afeat_var = Variable(afeat0)
+        target_var = Variable(target)
+
+        # if you have gpu, then shift data to GPU
+        if opt.cuda:
+            vfeat_var = vfeat_var.cuda()
+            afeat_var = afeat_var.cuda()
+            target_var = target_var.cuda()
+        sim = model(vfeat_var, afeat_var)
+        loss = criterion(sim, target_var)
+
+        loss_rec.append(list(loss.data)[0])
+        positive_rec.append(list(torch.mean(sim[0:bz - 1, 0]).data)[0])
+        negative_rec.append(list(torch.mean(sim[bz:bz * 2 - 1, 0]).data)[0])
+
+        # ##### for N pair loss
+        # vfeat = Variable(vfeat)
+        # afeat = Variable(afeat)
+        # if opt.cuda:
+        #     vfeat = vfeat.cuda()
+        #     afeat = afeat.cuda()
+        # bz = vfeat.size()[0]
+        # for k in np.arange(bz):
+        #     cur_vfeat = vfeat[k].clone()
+        #     vfeat_k = cur_vfeat.repeat(bz, 1, 1)
+        #     sim_k, dis1, dis2 = model(vfeat_k, afeat)
+        #     sim_k_0 = sim_k[:,0]
+        #     sim_k_1 = sim_k[:,1]
+        #     sim_k_0 = sim_k_0.resize(1,bz)
+        #     sim_k_1 = sim_k_1.resize(1,bz)
+        #     if k == 0:
+        #         sim_0 = sim_k_0.clone()
+        #         sim_1 = sim_k_1.clone()
+        #     else:
+        #         sim_0 = torch.cat((sim_0, sim_k_0), dim=0)
+        #         sim_1 = torch.cat((sim_1, sim_k_1), dim=0)
+        # loss = criterion(sim_0,sim_1)
+
+
+        # record the loss and distance to plot later
+        # dis1_rec.append(list(dis1.data)[0])
+        # dis2_rec.append(list(dis2.data)[0])
+
+        ##############################
+        # update loss in the loss meter
+        ##############################
+        losses.update(loss.data[0], vfeat.size(0))
+
+        ##############################
+        # compute gradient and do sgd
+        ##############################
+        optimizer.zero_grad()
+        loss.backward()
+
+        ##############################
+        # gradient clip stuff
+        ##############################
+        # utils.clip_gradient(optimizer, opt.gradient_clip)
+
+        # update parameters
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+        if i % opt.print_freq == 0:
+            log_str = 'No.{} Epoch: [{}][{}/{}]\t Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t Loss {loss.val:.4f} ({loss.avg:.4f})'.format(
+                num, epoch, i, len(train_loader), batch_time=batch_time, loss=losses)
+            print(log_str)
+
+
+def main():
+    global opt
+    # train data loader
+    tl_ls = []
+    for tds in tds_ls:
+        tl_ls.append(torch.utils.data.DataLoader(tds, batch_size=opt.batchSize,
+                                                 shuffle=True, num_workers=int(opt.workers)))
+
+    # create model
+    model_ls = []
+    for i in range(opt.model_number):
+        m = models.VAMetric_conv()
+        model_ls.append(m)
+
+    # if opt.init_model != '':
+    #     print('loading pretrained model from {0}'.format(opt.init_model))
+    #     model.load_state_dict(torch.load(opt.init_model))
+
+    # Contrastive Loss
+    criterion = models.conv_loss_dqy()
+    # criterion = models.N_pair_loss()
+
+    if opt.cuda:
+        print('shift model and criterion to GPU .. ')
+        for i in range(opt.model_number):
+            model_ls[i] = model_ls[i].cuda()
+        criterion = criterion.cuda()
+
+    # optimizer
+    # optimizer = optim.SGD(model.parameters(), lr=opt.lr,
+    #                      momentum=opt.momentum,
+    #                      weight_decay=opt.weight_decay)
+
+    opt_ls = []
+    for m in model_ls:
+        op = optim.Adam(m.parameters(), lr=opt.lr)
+        opt_ls.append(op)
+
+    # optimizer = optim.SGD(model.parameters(), lr=opt.lr, weight_decay=opt.weight_decay, momentum=opt.momentum)
+    # optimizer = optim.Adadelta(params=model.parameters(), lr=opt.lr)
+    # adjust learning rate every lr_decay_epoch
+    lambda_lr = lambda epoch: opt.lr_decay ** ((epoch + 1) // opt.lr_decay_epoch)  # poly policy
+    scheduler_ls = []
+    for op in opt_ls:
+        scheduler_ls.append(LR_Policy(op, lambda_lr))
+
+    resume_epoch = 0
+
+    global positive_rec
+    global negative_rec
+    global loss_rec
+
+    loss_rec = []
+    positive_rec = []
+    negative_rec = []
+
+    ######### to test each epoch
+    parser = OptionParser()
+    parser.add_option('--config',
+                      type=str,
+                      help="evaluation configuration",
+                      default="./configs/test_config.yaml")
+
+    (opts_test, args) = parser.parse_args()
+    opts_test = Config(opts_test.config)
+    test_video_dataset = VideoFeatDataset(root=opts_test.data_dir, flist=opts_test.video_flist, which_feat='vfeat',
+                                          creat_test=0)
+    test_audio_dataset = VideoFeatDataset(root=opts_test.data_dir, flist=opts_test.audio_flist, which_feat='afeat',
+                                          creat_test=0)
+    test_video_loader = torch.utils.data.DataLoader(test_video_dataset, batch_size=opts_test.batchSize,
+                                                    shuffle=False, num_workers=int(opts_test.workers))
+    test_audio_loader = torch.utils.data.DataLoader(test_audio_dataset, batch_size=opts_test.batchSize,
+                                                    shuffle=False, num_workers=int(opts_test.workers))
+
+    ########
+
+    # another test for git
+    for epoch in range(resume_epoch, opt.max_epochs):
+        #################################
+        # train for one epoch
+        #################################
+        for i in range(opt.model_number):
+            train(train_loader=tl_ls[i], model=model_ls[i], criterion=criterion, optimizer=opt_ls[i], epoch=epoch+1, opt=opt, num=i+1)
+            scheduler_ls[i].step()
+
+        ##################################
+        # save checkpoints
+        ##################################
+
+        # save model every 10 epochs
+        if ((epoch + 1) % opt.epoch_save) == 0:
+            for i in range(opt.model_number):
+                path_checkpoint = '{0}/{1}_state_epoch{2}_model{3}.pth'.format(opt.checkpoint_folder, opt.prefix,
+                                                                               epoch + 1, i)
+                utils.save_checkpoint(model_ls[i].state_dict(), path_checkpoint)
+        if ((epoch + 1) % opt.epoch_plot) == 0:
+            plt.figure(1)
+            plt.subplot(1, 2, 1)
+            plt.plot(loss_rec)
+            plt.legend('loss')
+            plt.subplot(1, 2, 2)
+            plt.plot(positive_rec)
+            plt.plot(negative_rec)
+            plt.legend(('simmilarity of positives', 'simmilarity of negatives'))
+            plt.show()
+            plt.savefig('./figures/result{0}.jpg'.format(epoch + 1))
+            plt.close()
+        if ((epoch + 1) % opt.epoch_test) == 0:
+            evaluate.test(test_video_loader, test_audio_loader, model_ls, opts_test)
+
+
+if __name__ == '__main__':
+    main()
