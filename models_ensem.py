88c2cb02a79d40b48527c12d0ac3294bf7785148
dongzeus <dongzeus@163.com>
2017-12-27 00:13:00 +0800

    m

--- models_ensem.py
+++ models_ensem.py
@@ -46,17 +46,17 @@ class VAMetric_conv(nn.Module):
         self.vLSTM = FeatLSTM(1024, 512, 128)
         self.aLSTM = FeatLSTM(128, 128, 128)
 
-        self.vfc1 = nn.Linear(in_features=1024, out_features=512)
+        self.vfc1 = nn.Linear(in_features=1024, out_features=128)
         self.vfc2 = nn.Linear(in_features=512, out_features=128)
         self.afc1 = nn.Linear(in_features=128, out_features=128)
         self.afc2 = nn.Linear(in_features=128, out_features=128)
 
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(2, 128), stride=128)  # output bn*32*120
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(2, 128), stride=128)  # output bn*32*120
         # self.mp = nn.MaxPool1d(kernel_size=4)
         self.dp = nn.Dropout(0.5)
-        self.conv2 = nn.Conv1d(in_channels=32, out_channels=32, kernel_size=8, stride=1)  # output bn*32*113
-        self.fc3 = nn.Linear(in_features=32 * 113, out_features=4096)
-        self.fc4 = nn.Linear(in_features=4096, out_features=2)
+        self.conv2 = nn.Conv1d(in_channels=8, out_channels=8, kernel_size=8, stride=1)  # output bn*32*113
+        self.fc3 = nn.Linear(in_features=8 * 113, out_features=1024)
+        self.fc4 = nn.Linear(in_features=1024, out_features=2)
         self.fc5 = nn.Linear(in_features=1024, out_features=2)
         self.fc6 = nn.Linear(in_features=128, out_features=2)
         self.init_params()
@@ -75,8 +75,8 @@ class VAMetric_conv(nn.Module):
 
         vfeat = self.vfc1(vfeat)
         vfeat = F.relu(vfeat)
-        vfeat = self.vfc2(vfeat)
-        vfeat = F.relu(vfeat)
+        # vfeat = self.vfc2(vfeat)
+        # vfeat = F.relu(vfeat)
 
         vfeat = vfeat.view(vfeat.size(0), 1, 1, -1)
         afeat = afeat.view(afeat.size(0), 1, 1, -1)
