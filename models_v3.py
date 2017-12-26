7bb02302fb77214d6f725f9532cfb4b11afac6ea
dongzeus <dongzeus@163.com>
2017-12-26 15:49:33 +0800

    ori

--- models_v3.py
+++ models_v3.py
@@ -120,7 +120,7 @@ class conv_loss_dqy(torch.nn.Module):
         # loss3 = 2 - (torch.mean(sim[0:length / 2 - 1]) - torch.mean(sim[length / 2:length - 1]))
         #
         # print(list(loss1.data)[0],list(loss2.data)[0],list(loss3.data)[0])
-        return (1 * loss1 + 0 * loss2 + 1 * loss3 + 0 * loss4)/2
+        return 1 * loss1 + 1 * loss3
 
 
 #
