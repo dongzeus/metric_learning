4ce5fb0015d9a090fe1e7b010beacabf3945e0f8
dongzeus <dongzeus@163.com>
2017-12-29 00:47:04 +0800

    mm

--- models_ensem.py
+++ models_ensem.py
@@ -118,7 +118,7 @@ class VA_LSTM(nn.Module):
 
         # vfeat = vfeat.view(vfeat.size(0), 1, 1, -1)
         # afeat = afeat.view(afeat.size(0), 1, 1, -1)
-        
+
         vfeat = F.relu(self.fc1(vfeat))
         vfeat = F.relu(self.fc2(vfeat))
         vfeat = F.relu(self.fc3(vfeat))
