a19495825a5bd50e72b6ddce153ef646aa211004
dongzeus <dongzeus@163.com>
2017-12-30 20:59:05 +0800

    mm

--- models_ensem.py
+++ models_ensem.py
@@ -156,10 +156,11 @@ class lstm_loss(nn.Module):
         sim_1 = sim[:, 1]
         loss_posi = torch.mean(F.relu(torch.pow(sim_1[0:bs / 2], 1)))
         loss_nega = torch.mean(F.relu(torch.pow(sim_0[bs / 2:bs], 1)))
+
         loss_balance = F.relu(
             0.9 - (torch.mean(torch.pow(sim_0[0:bs / 2], 2)) - torch.mean(torch.pow(sim_0[bs / 2:bs], 2))))
-        r = 0.9
-        loss = r * loss_nega + (1 - r) * loss_posi + 0.5 * loss_balance
+
+        loss = 0.1 * loss_nega + 0.1 * loss_posi + 1 * loss_balance
 
         print(loss_posi.data[0], loss_nega.data[0],loss_balance.data[0])
         return loss
