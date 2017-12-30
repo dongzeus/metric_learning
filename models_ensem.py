0da4d865f2021995395de820fffa8a43a9c90d5c
dongzeus <dongzeus@163.com>
2017-12-31 03:05:14 +0800

    m

--- models_ensem.py
+++ models_ensem.py
@@ -6,7 +6,6 @@ import numpy as np
 from optparse import OptionParser
 from tools.config_tools import Config
 
-
 parser = OptionParser()
 parser.add_option('--config',
                   type=str,
@@ -17,6 +16,7 @@ parser.add_option('--config',
 assert isinstance(opts, object)
 opt = Config(opts.config)
 USE_CUDA = opt.cuda
+
 
 class VAMetric_conv(nn.Module):
     def __init__(self, framenum=120):
@@ -91,18 +91,21 @@ class VA_lstm(nn.Module):
         if self.bidirection:
             self.num_direction = 2
 
-        self.vlstm = nn.LSTM(input_size=1024 * 3, hidden_size=self.hidden_size, num_layers=self.num_layers, dropout=0.3,
+        self.vlstm = nn.LSTM(input_size=1024 * 3, hidden_size=self.hidden_size, num_layers=self.num_layers, dropout=0.1,
                              batch_first=True, bidirectional=self.bidirection)
 
-        self.alstm = nn.LSTM(input_size=128 * 3, hidden_size=self.hidden_size, num_layers=self.num_layers, dropout=0.3,
+        self.alstm = nn.LSTM(input_size=128 * 3, hidden_size=self.hidden_size, num_layers=self.num_layers, dropout=0.1,
                              batch_first=True, bidirectional=self.bidirection)
 
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(2, 128 * 3 * 2),
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(2, 128 * 3 * 2),
                                stride=128 * 3 * 2)  # output bn * 16 * 118
+        self.mp1 = nn.MaxPool1d(kernel_size=3)  # bn * 16 * 39
+        self.conv2 = nn.Conv1d(in_channels=16, out_channels=16, kernel_size=4, stride=1)  # bn * 16 * 36
+        self.mp2 = nn.MaxPool1d(kernel_size=4)  # bn * 16 * 9
 
         self.dp = nn.Dropout(p=0.3)
-        self.vafc1 = nn.Linear(32 * 118, 1024)
-        self.vafc2 = nn.Linear(1024, 2)
+        self.vafc1 = nn.Linear(16 * 9, 128)
+        self.vafc2 = nn.Linear(128, 2)
         self.vafc3 = nn.Linear(1024, 2)
 
         self.Linear_init()
@@ -127,7 +130,11 @@ class VA_lstm(nn.Module):
 
         va = torch.cat((vlstm, alstm), dim=2)
         va = self.conv1(va)
+        va = va.view(va.size(0), va.size(1), -1)
         va = self.dp(va)
+        va = self.mp1(va)
+        va = self.conv2(va)
+        va = self.mp2(va)
         va = va.view(bs, -1)
 
         va = F.relu(self.vafc1(va))
@@ -155,6 +162,7 @@ class VA_lstm(nn.Module):
                 nn.init.xavier_normal(m.weight)
                 nn.init.constant(m.bias, 0)
 
+
 class lstm_loss(nn.Module):
     def __init__(self):
         super(lstm_loss, self).__init__()
@@ -176,45 +184,45 @@ class lstm_loss(nn.Module):
         print(loss_posi.data[0], loss_nega.data[0], loss_balance1.data[0], loss_balance2.data[0])
         return loss
 
-# class lstm_loss(nn.Module):
-#     def __init__(self):
-#         super(lstm_loss, self).__init__()
-#
-#     def forward(self, sim, target, margin=1):
-#         bs = sim.size(0)
-#         sim_0 = sim[:, 0]
-#         sim_1 = sim[:, 1]
-#         loss_posi = torch.mean(F.relu(torch.pow(sim_1[0:bs / 2], 1)))
-#         loss_nega = torch.mean(F.relu(torch.pow(sim_0[bs / 2:bs], 1)))
-#
-#         loss_balance1 = torch.mean(torch.clamp(margin - (sim_0[0:bs / 2] - sim_0[bs / 2:bs]), min=0))
-#
-#         loss_balance2 = F.relu(
-#             0.9 - (torch.mean(torch.pow(sim_1[bs / 2:bs], 1)) - torch.mean(torch.pow(sim_1[0:bs / 2], 1))))
-#
-#         loss = 0.1 * loss_nega + 0.1 * loss_posi + 1 * loss_balance1 + 0 * loss_balance2
-#
-#         print(loss_posi.data[0], loss_nega.data[0], loss_balance1.data[0])
-#         return loss
-#
+        # class lstm_loss(nn.Module):
+        #     def __init__(self):
+        #         super(lstm_loss, self).__init__()
+        #
+        #     def forward(self, sim, target, margin=1):
+        #         bs = sim.size(0)
+        #         sim_0 = sim[:, 0]
+        #         sim_1 = sim[:, 1]
+        #         loss_posi = torch.mean(F.relu(torch.pow(sim_1[0:bs / 2], 1)))
+        #         loss_nega = torch.mean(F.relu(torch.pow(sim_0[bs / 2:bs], 1)))
+        #
+        #         loss_balance1 = torch.mean(torch.clamp(margin - (sim_0[0:bs / 2] - sim_0[bs / 2:bs]), min=0))
+        #
+        #         loss_balance2 = F.relu(
+        #             0.9 - (torch.mean(torch.pow(sim_1[bs / 2:bs], 1)) - torch.mean(torch.pow(sim_1[0:bs / 2], 1))))
+        #
+        #         loss = 0.1 * loss_nega + 0.1 * loss_posi + 1 * loss_balance1 + 0 * loss_balance2
+        #
+        #         print(loss_posi.data[0], loss_nega.data[0], loss_balance1.data[0])
+        #         return loss
+        #
 
 
-    # def forward(self, sim, target, margin=0.5):
-    #     bs = sim.size(0)
-    #     sim_0 = sim[:, 0]
-    #     sim_1 = sim[:, 1]
-    #     posi_0 = sim_0[0:bs / 2]
-    #     posi_1 = sim_1[0:bs / 2]
-    #     nega_0 = sim_0[bs / 2:bs]
-    #     nega_1 = sim_1[bs / 2:bs]
-    #
-    #     loss_posi = torch.mean(F.relu(torch.pow(sim_1[0:bs / 2], 1)))
-    #     loss_nega = torch.mean(F.relu(torch.pow(sim_0[bs / 2:bs], 1)))
-    #
-    #     loss_balance1 = torch.mean(torch.pow(torch.clamp(margin - (posi_0 - nega_0), min=0), 1))
-    #     loss_balance2 = torch.mean(torch.pow(torch.clamp(margin - (nega_1 - posi_1), min=0), 1))
-    #
-    #     loss = 1 * loss_balance1 + 1 * loss_balance2
-    #
-    #     print(loss_balance1.data[0], loss_balance2.data[0])
-    #     return loss
+        # def forward(self, sim, target, margin=0.5):
+        #     bs = sim.size(0)
+        #     sim_0 = sim[:, 0]
+        #     sim_1 = sim[:, 1]
+        #     posi_0 = sim_0[0:bs / 2]
+        #     posi_1 = sim_1[0:bs / 2]
+        #     nega_0 = sim_0[bs / 2:bs]
+        #     nega_1 = sim_1[bs / 2:bs]
+        #
+        #     loss_posi = torch.mean(F.relu(torch.pow(sim_1[0:bs / 2], 1)))
+        #     loss_nega = torch.mean(F.relu(torch.pow(sim_0[bs / 2:bs], 1)))
+        #
+        #     loss_balance1 = torch.mean(torch.pow(torch.clamp(margin - (posi_0 - nega_0), min=0), 1))
+        #     loss_balance2 = torch.mean(torch.pow(torch.clamp(margin - (nega_1 - posi_1), min=0), 1))
+        #
+        #     loss = 1 * loss_balance1 + 1 * loss_balance2
+        #
+        #     print(loss_balance1.data[0], loss_balance2.data[0])
+        #     return loss
