b0a27e64fe89719128fea1532a3b04f3eddb96e4
dongzeus <dongzeus@163.com>
2017-12-30 20:15:40 +0800

    lstm

--- models_ensem.py
+++ models_ensem.py
@@ -70,7 +70,7 @@ class VAMetric_conv(nn.Module):
 
 
 class VA_lstm(nn.Module):
-    def __init__(self, hidden_size=128, num_layers=5):
+    def __init__(self, hidden_size=128 * 3, num_layers=5):
         super(VA_lstm, self).__init__()
 
         self.hidden_size = hidden_size
@@ -81,49 +81,45 @@ class VA_lstm(nn.Module):
             self.num_direction = 2
         self.valstm_hidden_size = self.hidden_size * 2 * self.num_direction
 
-        self.vlstm = nn.LSTM(input_size=1024, hidden_size=self.hidden_size, num_layers=self.num_layers, dropout=0.1,
+        self.vlstm = nn.LSTM(input_size=1024 * 3, hidden_size=self.hidden_size, num_layers=self.num_layers, dropout=0.1,
                              batch_first=True, bidirectional=self.bidirection)
 
-        self.alstm = nn.LSTM(input_size=128, hidden_size=self.hidden_size, num_layers=self.num_layers, dropout=0.1,
+        self.alstm = nn.LSTM(input_size=128 * 3, hidden_size=self.hidden_size, num_layers=self.num_layers, dropout=0.1,
                              batch_first=True, bidirectional=self.bidirection)
 
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(2, 128), stride=128)  # output bn * 32 * 120
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(2, 128 * 3),
+                               stride=128 * 3)  # output bn * 16 * 118
+
         self.dp = nn.Dropout(p=0.5)
-        self.vfc = nn.Linear(1024, 128)
-        self.fc1 = nn.Linear(120 * 32, 1024)
-        self.fc2 = nn.Linear(1024, 1024)
-        self.fc3 = nn.Linear(1024, 2)
-        self.vafc1 = nn.Linear(256, 256)
-        self.vafc2 = nn.Linear(256, 256)
-        self.vafc3 = nn.Linear(256, 2)
+        self.vafc1 = nn.Linear(16 * 118, 1024)
+        self.vafc2 = nn.Linear(1024, 2)
 
         self.Linear_init()
 
     def forward(self, vfeat, afeat):
         bs = vfeat.size(0)
+        for seq in range(117):
+            if seq == 0:
+                vfeat_3 = vfeat[:, seq:seq + 2, :].view(bs, 1, -1)
+                afeat_3 = afeat[:, seq:seq + 2, :].view(bs, 1, -1)
 
-        vfeat = self.vlstm(vfeat, self.param_init(batch_size=bs))[0]
-        afeat = self.alstm(afeat, self.param_init(batch_size=bs))[0]
+            else:
+                vfeat_3 = torch.cat((vfeat_3, vfeat[:, seq:seq + 2, :].view(bs, 1, -1)), dim=1)
+                afeat_3 = torch.cat((afeat_3, afeat[:, seq:seq + 2, :].view(bs, 1, -1)), dim=1)
 
-        vfeat = vfeat[:, 119, :]
-        afeat = afeat[:, 119, :]
-        va = torch.cat((vfeat, afeat), dim=1)
+        vlstm = self.vlstm(vfeat_3, self.param_init(batch_size=bs, hidden_size=self.hidden_size))[0]
+        alstm = self.alstm(afeat_3, self.param_init(batch_size=bs, hidden_size=self.hidden_size))[0]
+
+        vlstm = vlstm.resize(bs, 1, 1, 118 * self.hidden_size * 3)
+        alstm = alstm.resize(bs, 1, 1, 118 * self.hidden_size * 3)
+
+        va = torch.cat((vlstm, alstm), dim=2)
+        va = self.conv1(va)
+        va = self.dp(va)
+        va = va.view(bs, -1)
+
         va = F.relu(self.vafc1(va))
-        va = F.relu(self.vafc2(va))
-        sim = F.softmax(self.vafc3(va))
-
-        #
-        # vfeat = F.relu(self.vfc(vfeat))
-        # vfeat = vfeat.resize(bs, 1, 1, 120 * 128)
-        # afeat = afeat.resize(bs, 1, 1, 120 * 128)
-        # vafeat = torch.cat((vfeat, afeat), dim=2)
-        # vafeat = self.conv1(vafeat)
-        # vafeat = self.dp(vafeat)
-
-        # vafeat = vafeat.view(bs, -1)
-        # vafeat = F.relu(self.fc1(vafeat))
-        # vafeat = F.relu(self.fc2(vafeat))
-        # sim = F.softmax(self.fc3(vafeat))
+        sim = F.softmax(self.vafc2(va))
 
         return sim
 
