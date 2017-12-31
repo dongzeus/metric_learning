b75f95d50a2fab92642f127e4fd6e4423c46f8e2
dongzeus <dongzeus@163.com>
2017-12-31 13:12:15 +0800

    mm

--- models_ensem.py
+++ models_ensem.py
@@ -80,85 +80,81 @@ class VAMetric_conv(nn.Module):
         return result
 
 
-# class VA_lstm(nn.Module):
-#     def __init__(self, hidden_size=128 * 3, num_layers=2):
-#         super(VA_lstm, self).__init__()
-#
-#         self.hidden_size = hidden_size
-#         self.num_layers = num_layers
-#         self.bidirection = True
-#         self.num_direction = 1
-#         if self.bidirection:
-#             self.num_direction = 2
-#
-#         self.vlstm = nn.LSTM(input_size=1024 * 3, hidden_size=self.hidden_size, num_layers=self.num_layers, dropout=0.1,
-#                              batch_first=True, bidirectional=self.bidirection)
-#
-#         self.alstm = nn.LSTM(input_size=128 * 3, hidden_size=self.hidden_size, num_layers=self.num_layers, dropout=0.1,
-#                              batch_first=True, bidirectional=self.bidirection)
-#
-#         self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(2, 128 * 3 * 2),
-#                                stride=128 * 3 * 2)  # output bn * 16 * 118
-#         self.mp1 = nn.MaxPool1d(kernel_size=3)  # bn * 16 * 39
-#         self.conv2 = nn.Conv1d(in_channels=8, out_channels=8, kernel_size=4, stride=1)  # bn * 16 * 36
-#
-#         self.dp = nn.Dropout(p=0.3)
-#         self.vafc1 = nn.Linear(8 * 36, 256)
-#         self.vafc2 = nn.Linear(256, 2)
-#         self.vafc3 = nn.Linear(1024, 2)
-#
-#         self.Linear_init()
-#
-#     def forward(self, vfeat, afeat):
-#         bs = vfeat.size(0)
-#
-#         for seq in range(118):
-#             if seq == 0:
-#                 vfeat_3 = vfeat[:, seq:seq + 3, :].resize(bs, 1, 3 * 1024)
-#                 afeat_3 = afeat[:, seq:seq + 3, :].resize(bs, 1, 3 * 128)
-#
-#             else:
-#                 vfeat_3 = torch.cat((vfeat_3, vfeat[:, seq:seq + 3, :].resize(bs, 1, 3 * 1024)), dim=1)
-#                 afeat_3 = torch.cat((afeat_3, afeat[:, seq:seq + 3, :].resize(bs, 1, 3 * 128)), dim=1)
-#
-#         vlstm = self.vlstm(vfeat_3, self.param_init(batch_size=bs, hidden_size=self.hidden_size))[0]
-#         alstm = self.alstm(afeat_3, self.param_init(batch_size=bs, hidden_size=self.hidden_size))[0]
-#
-#         vlstm = vlstm.resize(bs, 1, 1, 118 * self.hidden_size * 2)
-#         alstm = alstm.resize(bs, 1, 1, 118 * self.hidden_size * 2)
-#
-#         va = torch.cat((vlstm, alstm), dim=2)
-#         va = self.conv1(va)
-#         va = va.view(va.size(0), va.size(1), -1)
-#         va = self.dp(va)
-#         va = self.mp1(va)
-#         va = self.conv2(va)
-#         va = va.view(bs, -1)
-#
-#         va = F.relu(self.vafc1(va))
-#         sim = F.softmax(self.vafc2(va))
-#
-#         return sim
-#
-#     def param_init(self, batch_size, hidden_size=None):
-#         if hidden_size is None:
-#             hidden_size = self.hidden_size
-#         bs = batch_size
-#         h_0 = Variable(torch.zeros(self.num_layers * self.num_direction, bs, hidden_size))
-#         c_0 = Variable(torch.zeros(self.num_layers * self.num_direction, bs, hidden_size))
-#         torch.nn.init.xavier_normal(h_0)
-#         torch.nn.init.xavier_normal(c_0)
-#         if USE_CUDA:
-#             h_0 = h_0.cuda()
-#             c_0 = c_0.cuda()
-#
-#         return h_0, c_0
-#
-#     def Linear_init(self):
-#         for m in self.modules():
-#             if isinstance(m, nn.Linear):
-#                 nn.init.xavier_normal(m.weight)
-#                 nn.init.constant(m.bias, 0)
+class VA_lstm(nn.Module):
+    def __init__(self, hidden_size=128 * 3, num_layers=2):
+        super(VA_lstm, self).__init__()
+
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.bidirection = True
+        self.num_direction = 1
+        if self.bidirection:
+            self.num_direction = 2
+
+        self.vlstm = nn.LSTM(input_size=1024 * 3, hidden_size=self.hidden_size, num_layers=self.num_layers, dropout=0.1,
+                             batch_first=True, bidirectional=self.bidirection)
+
+        self.alstm = nn.LSTM(input_size=128 * 3, hidden_size=self.hidden_size, num_layers=self.num_layers, dropout=0.1,
+                             batch_first=True, bidirectional=self.bidirection)
+
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(2, 128 * 3 * 2),
+                               stride=128 * 3 * 2)  # output bn * 16 * 118
+        # self.mp1 = nn.MaxPool1d(kernel_size=3)  # bn * 16 * 39
+        # self.conv2 = nn.Conv1d(in_channels=8, out_channels=8, kernel_size=4, stride=1)  # bn * 16 * 36
+
+        self.dp = nn.Dropout(p=0.3)
+        self.vafc1 = nn.Linear(16 * 118, 1024)
+        self.vafc2 = nn.Linear(1024, 2)
+        self.vafc3 = nn.Linear(1024, 2)
+
+        self.Linear_init()
+
+    def forward(self, vfeat, afeat):
+        bs = vfeat.size(0)
+
+        for seq in range(118):
+            if seq == 0:
+                vfeat_3 = vfeat[:, seq:seq + 3, :].resize(bs, 1, 3 * 1024)
+                afeat_3 = afeat[:, seq:seq + 3, :].resize(bs, 1, 3 * 128)
+
+            else:
+                vfeat_3 = torch.cat((vfeat_3, vfeat[:, seq:seq + 3, :].resize(bs, 1, 3 * 1024)), dim=1)
+                afeat_3 = torch.cat((afeat_3, afeat[:, seq:seq + 3, :].resize(bs, 1, 3 * 128)), dim=1)
+
+        vlstm = self.vlstm(vfeat_3, self.param_init(batch_size=bs, hidden_size=self.hidden_size))[0]
+        alstm = self.alstm(afeat_3, self.param_init(batch_size=bs, hidden_size=self.hidden_size))[0]
+
+        vlstm = vlstm.resize(bs, 1, 1, 118 * self.hidden_size * 2)
+        alstm = alstm.resize(bs, 1, 1, 118 * self.hidden_size * 2)
+
+        va = torch.cat((vlstm, alstm), dim=2)
+        va = self.conv1(va)
+        va = va.view(bs, -1)
+
+        va = F.relu(self.vafc1(va))
+        sim = F.softmax(self.vafc2(va))
+
+        return sim
+
+    def param_init(self, batch_size, hidden_size=None):
+        if hidden_size is None:
+            hidden_size = self.hidden_size
+        bs = batch_size
+        h_0 = Variable(torch.zeros(self.num_layers * self.num_direction, bs, hidden_size))
+        c_0 = Variable(torch.zeros(self.num_layers * self.num_direction, bs, hidden_size))
+        torch.nn.init.xavier_normal(h_0)
+        torch.nn.init.xavier_normal(c_0)
+        if USE_CUDA:
+            h_0 = h_0.cuda()
+            c_0 = c_0.cuda()
+
+        return h_0, c_0
+
+    def Linear_init(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_normal(m.weight)
+                nn.init.constant(m.bias, 0)
 
 class VA_lstm(nn.Module):
     def __init__(self, hidden_size=128 * 3, num_layers=2):
