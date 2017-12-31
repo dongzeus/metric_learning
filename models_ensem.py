1630c2d0d11239ec49ef28f90d766096d22ab160
dongzeus <dongzeus@163.com>
2017-12-31 21:42:19 +0800

    mm

--- models_ensem.py
+++ models_ensem.py
@@ -97,13 +97,13 @@ class VA_lstm(nn.Module):
         self.alstm = nn.LSTM(input_size=128 * 3, hidden_size=self.hidden_size, num_layers=self.num_layers, dropout=0.1,
                              batch_first=True, bidirectional=self.bidirection)
 
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(2, 128 * 3 * 2),
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(2, 128 * 3 * 2*2),
                                stride=128 * 3 * 2)  # output bn * 16 * 118
         # self.mp1 = nn.MaxPool1d(kernel_size=3)  # bn * 16 * 39
         # self.conv2 = nn.Conv1d(in_channels=8, out_channels=8, kernel_size=4, stride=1)  # bn * 16 * 36
 
         self.dp = nn.Dropout(p=0.3)
-        self.vafc1 = nn.Linear(16 * 118, 1024)
+        self.vafc1 = nn.Linear(16 * 117, 1024)
         self.vafc2 = nn.Linear(1024, 2)
         self.vafc3 = nn.Linear(1024, 2)
 
